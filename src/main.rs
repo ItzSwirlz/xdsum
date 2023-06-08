262868c9f83d0dc9f33b6f7bd6234510a70cd268
Joshua Peisach <itzswirlz2020@outlook.com>
2023-06-08 13:17:09 -0400

    Dont implement our own sum, give user the option + implement mode
    i'm not smart enough with hash functions to make my own, besides, sometimes
    one sum is better than other. lets let the user pick one
    
    also: implement mode

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,32 @@
 use clap::Parser;
-use std::io::Read;
-use std::ops::{Shr, Index};
-use std::{fs::File, path::PathBuf};
-use std::path::{self, Path};
+use std::fs::*;
+use std::fs;
+use std::io::*;
+use std::os::unix::prelude::PermissionsExt;
+use std::path::*;
+use sha256;
 
 #[derive(Parser)]
 struct Cli {
+    pub method: String,
     pub file: std::path::PathBuf,
 }
 
 fn main() {
     let args = Cli::parse();
-    
-    let path = File::open(args.file);
-    let bytes: &mut Vec<u8> = &mut vec![];
-    path.unwrap().read_to_end(bytes).unwrap();
 
-    let mut summed = bytes.clone().to_owned();
-    for i in bytes.iter().enumerate() {
-        if(i.0 == bytes.len() - 1) {
-            break;
-        }
-        summed[i.0 + 1] *= i.1;
-    }
-    println!("{:#?}", summed);
-    
-    let ret: &mut Vec<char> = &mut vec![];
-    let mut count = 0;
-    for mut i in summed.clone() {
-        i >>= count;
-        count += 1;
-        if(i.is_ascii_alphanumeric()) {
-            ret.push(i as char);
-        }
+    let mut tmp_file = File::create("xdsum.tmp").expect("Failed to create temp file");
+    tmp_file.write(fs::read_to_string(args.file.clone()).expect("Failed to read input file").as_bytes()).expect("Failed to write to the temp file");
+
+    if args.method.eq("sha256") || args.method.eq("sha256sum") {
+        // get metadata
+        let metadata = metadata(args.file).expect("Failed to get file metadata");
+
+        // file mode
+        tmp_file.write(&[metadata.permissions().mode() as u8]).expect("Failed to write permission nodes to the temp file");
+        println!("{}", sha256::try_digest(Path::new("./xdsum.tmp")).unwrap());
     }
     
-    println!("{}", ret.into_iter().map(|i| i.to_string()).collect::<String>());
+    // remove the tmp file
+    fs::remove_file("xdsum.tmp").expect("Failed to remove the temp file");
 }
